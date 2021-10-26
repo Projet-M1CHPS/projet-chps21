9bf45ef1e77f55f89f3c8f746ab4be682c987f1e
byjtew93 <benjamin.lozes@protonmail.com>
2021-10-26 15:21:54 +0200

    Cleaning

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -3,13 +3,16 @@
 #include <cassert>
 #include <iostream>
 
-using namespace image;
+const unsigned possible_brightness = 255 * 3;
 
-std::array<long double, POSSIBLE_BRIGHTNESSES> _createBrightnessHeightmap(
+namespace image::transform {
+
+namespace {
+std::array<long double, possible_brightness> _createBrightnessHeightmap(
     Image const &image) {
     std::cout << "> _getBrightnessHeightmap()" << std::endl;
     long double increment_value = 1.0 / image.width * image.height;
-    std::array<long double, POSSIBLE_BRIGHTNESSES> heightmap;
+    std::array<long double, possible_brightness> heightmap;
 
     heightmap.fill(0.0);
     for (Color each : image.colors) {
@@ -19,9 +22,10 @@ std::array<long double, POSSIBLE_BRIGHTNESSES> _createBrightnessHeightmap(
     std::cout << "< _getBrightnessHeightmap()" << std::endl;
     return heightmap;
 }
+}  // namespace
 
-bool transform::BlackWhiteScale::transform(Image &image) {
-    std::array<long double, POSSIBLE_BRIGHTNESSES> heightmap =
+bool BlackWhiteScale::transform(Image &image) {
+    std::array<long double, possible_brightness> heightmap =
         _createBrightnessHeightmap(image);
     unsigned long dimension = image.width * image.height;
 
@@ -33,21 +37,24 @@ bool transform::BlackWhiteScale::transform(Image &image) {
     return true;
 }
 
-bool transform::GreyScale::transform(Image &image) {
-    std::cout << "> transform::GreyScale::transform()" << std::endl;
+bool GreyScale::transform(Image &image) {
+    std::cout << "> GreyScale::transform()" << std::endl;
     unsigned long dimension = image.width * image.height;
 
     for (unsigned i = 0; i < dimension; i++) {
-        unsigned mean = (unsigned) ((image.colors[i].r+image.colors[i].g+image.colors[i].b)/3.0);
-        if(mean < 0)
+        unsigned mean = (unsigned)((image.colors[i].r + image.colors[i].g +
+                                    image.colors[i].b) /
+                                   3.0);
+        if (mean < 0)
             mean = 0;
-        else if(mean > 255)
+        else if (mean > 255)
             mean = 255;
         image.colors[i].r = mean;
         image.colors[i].g = mean;
         image.colors[i].b = mean;
     }
-    //image.print();
-    std::cout << "< transform::GreyScale::transform()" << std::endl;
+    // image.print();
+    std::cout << "< GreyScale::transform()" << std::endl;
     return true;
 }
+}  // namespace image::transform
--- src/TransformEngine.cpp
+++ src/TransformEngine.cpp
@@ -3,33 +3,23 @@
 
 #include "Transform.hpp"
 
-void image::transform::TransformEngine::insertTransformation(
+namespace image::transform {
+
+void TransformEngine::insertTransformation(
     size_t position, std::shared_ptr<Transformation> transformation) {
-    this->transformations.insert(this->transformations.begin() + position,
-                                 transformation);
+    transformations.insert(transformations.begin() + position, transformation);
 }
 
-void image::transform::TransformEngine::addTransformation(
+void TransformEngine::addTransformation(
     std::shared_ptr<Transformation> transformation) {
-    std::cout << "> TransformEngine::addTransformation()" << std::endl;
-    this->transformations.push_back(transformation);
-    std::cout << "< TransformEngine::addTransformation()" << std::endl;
+    transformations.push_back(transformation);
 }
 
-image::Image image::transform::TransformEngine::transform(
-    image::Image const &image) {
-    std::cout << "> TransformEngine::transform()" << std::endl;
-    image::Image copy(image.width, image.height,
-                      std::vector<Color>(image.colors));
-    image::Image& copy_ref = copy;
-    for (size_t i = 0; i < this->transformations.size(); i++) {
-        std::cout << ">> Transformation[" << i << "]" << std::endl;
-        std::shared_ptr<image::transform::Transformation> each_tr =
-            this->transformations[i];
-        image::transform::Transformation *fct = each_tr.get();
-        fct->transform(copy_ref);
-        std::cout << "<< Transformation[" << i << "]" << std::endl;
-    }
-    std::cout << "< TransformEngine::transform()" << std::endl;
+Image TransformEngine::transform(Image const &image) {
+    Image copy(image);
+    for (std::shared_ptr<Transformation> tr : transformations)
+        tr->transform(copy);
     return copy;
 }
+
+}  // namespace image::transform
