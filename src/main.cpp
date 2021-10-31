401a44fd68e5421185a0a7eab3c75e8499986dda
johnkyky <ugobattiston@gmail.com>
2021-10-31 15:28:57 +0100

    ajout d'une fonction pour tester a la main le reseau de neuronne sur un probleme basique comme le xor

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,67 @@
 #include "ActivationFunction.hpp"
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <array>
+
+template <typename T>
+void func_xor()
+{
+  nnet::NeuralNetwork<T> nn;
+  nn.setLayersSize(std::vector<size_t>{2, 2, 1});
+  nn.setActivationFunction(af::ActivationFunctionType::sigmoid);
+  nn.randomizeSynapses();
+
+  auto &b = nn.getBiases();
+
+  for (auto &i : b)
+    for (auto &e : i)
+      e = 0;
+
+  std::cout << nn << std::endl;
+
+  std::vector<std::vector<T>> input{{1, 1},
+                                    {1, 0},
+                                    {0, 1},
+                                    {0, 0}};
+  std::vector<T> target{0, 1, 1, 0};
+
+  float error = 1.f;
+  size_t count = 0;
+  while (error > 0.02)
+  {
+    for (int i = 0; i < 10000; i++)
+      for (int j = 0; j < 4; j++)
+        nn.train_bis(input[j].begin(), input[j].end(), target.begin() + j, target.begin() + j + 1, 0.075);
+
+    error = 0.0;
+    for (int i = 0; i < 4; i++)
+      error += std::fabs(nn.forward(input[i].begin(), input[i].end())(0, 0) - target[i]);
+    error /= 4;
+    std::cout << error << std::endl;
+    count++;
+  }
+
+  std::cout << "Result" << "--->" << count << std::endl;
+  for (int i = 0; i < 4; i++)
+  {
+    std::cout << input[i][0] << "|" << input[i][1] << " = " << target[i] << std::endl;
+    std::cout << nn.forward(input[i].begin(), input[i].end()) << std::endl;
+  }
+  std::cout << nn << std::endl;
+}
 
 // Test main, should be replaced by the python interface
 int main(int argc, char **argv)
 {
   nnet::NeuralNetwork<float> nn;
   nn.setLayersSize(std::vector<size_t>{2, 2, 1});
-  nn.setActivationFunction(af::ActivationFunctionType::square);
+  nn.setActivationFunction(af::ActivationFunctionType::sigmoid);
   nn.randomizeSynapses();
+  //nn.setActivationFunction(af::ActivationFunctionType::identity, 2);
 
   auto &w = nn.getWeights();
   auto &b = nn.getBiases();
-
-  for (auto &i : w)
-  {
-    for (auto &e : i)
-      e = 1.f;
-    //std::cout << i << std::endl;
-  }
 
   for (auto &i : b)
   {
@@ -32,20 +75,11 @@ int main(int argc, char **argv)
   }
 
   std::vector<float> input{1, 1};
-  std::vector<float> target{0};
-  
-  nn.train_bis(input.begin(), input.end(), target.begin(), target.end());
+  std::vector<float> target{-6.255, 20003};
 
-  /*std::cout << "forward :\n" << nn.forward(input.begin(), input.end());
-  for(int i = 0; i < 1000000; i++)
-    nn.train(input.begin(), input.end(), target.begin(), target.end());
-
-  std::cout << "forward :\n" << nn.forward(input.begin(), input.end());
-
-  for(int i = 0; i < 1000000; i++)
-    nn.train(input.begin(), input.end(), target.begin(), target.end());
-
-  std::cout << "forward :\n" << nn.forward(input.begin(), input.end()); */
+  func_xor<float>();
+  //printf("--------------------\n");
+  //func_xor<double>();
 
   return 0;
 }
