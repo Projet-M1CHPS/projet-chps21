5f91d9c97af04e092f473f3104dce33099784567
byjtew93 <benjamin.lozes@protonmail.com>
2021-11-15 22:39:00 +0100

    Adding a new Image test
    Check if comparison between two different dimensioned images is possible.

--- test/Image_test.cpp
+++ test/Image_test.cpp
@@ -2,3 +2,9 @@
 
 #include <gtest/gtest.h>
 
+TEST(ImageTest, DifferenceBetweenDifferentImageDimensions) {
+    image::GrayscaleImage a = image::ImageLoader::createRandomNoiseImage(124, 108);
+    image::GrayscaleImage b = image::ImageLoader::createRandomNoiseImage(96, 32);
+    // Add a simple transformation
+    ASSERT_NO_THROW(a.getDifference(b));
+}
