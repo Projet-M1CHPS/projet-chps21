c897b0a15cbd25be398408673c42335aad642dd1
byjtew93 <benjamin.lozes@protonmail.com>
2021-11-30 16:12:52 +0100

    New tests for Transformations & reformat.
    New tests for resizing on forbidden dimensions.
    
    Reformating tests names.

--- test/Transform_test.cpp
+++ test/Transform_test.cpp
@@ -8,10 +8,8 @@ TEST(TransformTest, HistogramInversionLossless) {
   image::GrayscaleImage original = image::ImageSerializer::createRandomNoiseImage();
   // Add a simple transformation
   image::transform::TransformEngine te;
-  te.addTransformation(
-          std::make_shared<image::transform::Inversion>());
-  te.addTransformation(
-          std::make_shared<image::transform::Inversion>());
+  te.addTransformation(std::make_shared<image::transform::Inversion>());
+  te.addTransformation(std::make_shared<image::transform::Inversion>());
   image::GrayscaleImage edited = te.transform(original);
   double diff = original.getDifference(edited);
   ASSERT_FLOAT_EQ(diff, .0);
@@ -28,7 +26,7 @@ TEST(TransformTest, DoubleBinaryScaleLossless) {
   ASSERT_FLOAT_EQ(diff, .0);
 }
 
-TEST(ScaleDownSameDimensionTest, DoubleBinaryScaleLossless) {
+TEST(TransformTest, ResizeInSquare) {
   image::GrayscaleImage original = image::ImageSerializer::createRandomNoiseImage(50, 50);
   // Add a simple transformation
   image::transform::Resize resize_tranform = image::transform::Resize(10, 10);
@@ -37,10 +35,21 @@ TEST(ScaleDownSameDimensionTest, DoubleBinaryScaleLossless) {
 }
 
 
-TEST(ScaleDownDifferentDimensionTest, DoubleBinaryScaleLossless) {
+TEST(TransformTest, ResizeInRectangle) {
   image::GrayscaleImage original = image::ImageSerializer::createRandomNoiseImage(50, 50);
   // Add a simple transformation
   image::transform::Resize resize_tranform = image::transform::Resize(10, 25);
   ASSERT_NO_THROW(resize_tranform.transform(original));
   ASSERT_TRUE(original.getWidth() == 10 && original.getHeight() == 25);
+}
+
+TEST(TransformTest, ResizeToZero) {
+  image::GrayscaleImage original = image::ImageSerializer::createRandomNoiseImage(50, 50);
+  // Add a simple transformation
+  image::transform::Resize resize_tranform = image::transform::Resize(0, 25);
+  ASSERT_THROW(resize_tranform.transform(original), std::invalid_argument);
+  resize_tranform = image::transform::Resize(25, 0);
+  ASSERT_THROW(resize_tranform.transform(original), std::invalid_argument);
+  resize_tranform = image::transform::Resize(0, 0);
+  ASSERT_THROW(resize_tranform.transform(original), std::invalid_argument);
 }
