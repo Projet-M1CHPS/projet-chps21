7d61b987891b1cec1ebd3dbe1ece0ff44cadea4f
M5ssss <arthur.herblot@gmail.com>
2021-11-30 16:15:35 +0100

    Added more image tests
    [+] image constructor test
    [+] image load test
    [+] image directory load tests
    [+] image getters tests
    [+] image setters tests

--- test/Image_test.cpp
+++ test/Image_test.cpp
@@ -2,6 +2,13 @@
 
 #include <gtest/gtest.h>
 
+
+TEST(ImageTest, CreateImageFromAnotherImage) {
+    image::GrayscaleImage a = image::ImageSerializer::createRandomNoiseImage(128, 128);
+    image::GrayscaleImage b{a};
+    ASSERT_EQ(a.getDifference(b), 0.0);
+}
+
 TEST(ImageTest, DifferenceBetweenDifferentImageDimensions) {
     image::GrayscaleImage a = image::ImageSerializer::createRandomNoiseImage(124, 108);
     image::GrayscaleImage b = image::ImageSerializer::createRandomNoiseImage(96, 32);
@@ -15,13 +22,18 @@ TEST(ImageTest, SaveAndLoadRandomImage) {
     image::GrayscaleImage a = image::ImageSerializer::createRandomNoiseImage(128,128);
     image::ImageSerializer::save("SaveAndLoadRandomImage_TestDir/testImage.png", a);
     image::GrayscaleImage b = image::ImageSerializer::load("SaveAndLoadRandomImage_TestDir/testImage.png");
-    ASSERT_EQ(a.getDifference(b), 0.0);
-
+    ASSERT_EQ(a.getDifference(b), 0.0); // the images are similar -> save and load are working
+    ASSERT_ANY_THROW(image::ImageSerializer::load("thisImageDoesntExist"));
     std::filesystem::remove_all("SaveAndLoadRandomImage_TestDir"); // delete the test dir
 }
 
 TEST(ImageTest, LoadPngDirectory) {
+    
+    ASSERT_ANY_THROW(image::ImageSerializer::loadDirectory("Image_test")); // error, the file isnt a directory
+    ASSERT_ANY_THROW(image::ImageSerializer::loadDirectory("LoadPngDirectory_TestDir")); // error, the directory doesnt exist
     std::filesystem::create_directory("LoadPngDirectory_TestDir"); //create a dir to store the test images
+
+    ASSERT_ANY_THROW(image::ImageSerializer::loadDirectory("LoadPngDirectory_TestDir")); // warning, the directory is empty
 
     image::GrayscaleImage a = image::ImageSerializer::createRandomNoiseImage(128,128);
     image::GrayscaleImage b = image::ImageSerializer::createRandomNoiseImage(128,128);
@@ -41,21 +53,52 @@ TEST(ImageTest, LoadPngDirectory) {
     std::filesystem::remove_all("LoadPngDirectory_TestDir"); // delete the test dir
 }
 
-/* TESTS TO ADD
- *
- * - Constructors
- * -- init a grayscale image from another image, assert they arent different
- * 
- * - Getters
- * -- getPixel 1d and 2d
- * -- getData
- * -- getHistogramm
- * -- begins, ends, assert the complete iteration through the image
- * -- getDimension, size, width and height
- * 
- * - Set
- * -- change value of several pixels, assert they match the wanted values
- *
- * - Errors
- * -- add and test errors in 
- */ 
+TEST(ImageTest, ImageGetters) {
+    image::GrayscaleImage a = image::ImageSerializer::createRandomNoiseImage(128,64);
+    // test most getters
+    ASSERT_EQ(a.getWidth(), 128);
+    ASSERT_EQ(a.getHeight(), 64);
+    ASSERT_EQ(a.getSize(), 128 * 64);
+    ASSERT_EQ(a.getDimension().first,128);
+    ASSERT_EQ(a.getDimension().second,64);
+    image::grayscale_t* pixel_data_ptr = a.getData();
+    for (unsigned i = 0; i < a.getSize(); i++) {
+        ASSERT_EQ(pixel_data_ptr[i], a.getPixel(i));
+    }
+
+    // special histogram tests
+    std::unique_ptr<image::grayscale_t[]> pixel_data_ptr2 = std::make_unique<image::grayscale_t[]>(6);
+    pixel_data_ptr2[0] = 0;
+    pixel_data_ptr2[1] = 255;
+    pixel_data_ptr2[2] = 0;
+    pixel_data_ptr2[3] = 1;
+    pixel_data_ptr2[4] = 0;
+    pixel_data_ptr2[5] = 255;
+    image::GrayscaleImage b{6,1,std::move(pixel_data_ptr2)};
+    std::vector<size_t> bHist = b.getHistogram();
+    ASSERT_EQ(bHist[0], 3);
+    ASSERT_EQ(bHist[1], 1);
+    ASSERT_EQ(bHist[255], 2);
+}
+
+TEST(ImageTest, ImageSetters) {
+    std::unique_ptr<image::grayscale_t[]> pixel_data = std::make_unique<image::grayscale_t[]>(4);
+    pixel_data[0] = 0;
+    pixel_data[1] = 0;
+    pixel_data[2] = 0;
+    pixel_data[3] = 0;
+    image::GrayscaleImage a{2,2,std::move(pixel_data)};
+
+    image::grayscale_t* pixel_data_ptr = a.getData();
+    // changes and assert the change of some pixels
+    pixel_data_ptr[0] = 255;
+    pixel_data_ptr[2] = 255;
+    ASSERT_EQ(a.getPixel(0), 255);
+    ASSERT_EQ(a.getPixel(0,1), 255);
+
+    // resize the image (values arent kept)
+    a.setSize(4,4);
+    ASSERT_EQ(a.getWidth(), 4);
+    ASSERT_EQ(a.getHeight(), 4);
+    ASSERT_EQ(a.getSize(), 4*4);
+}
