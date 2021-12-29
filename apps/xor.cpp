95a93d9197bd0ba5171c2bb9f0133c7773a5f8b0
JAM Mathys <mathys.jam@ens.uvsq.fr>
2021-12-29 11:52:35 +0100

    Fixed xor testing app

--- apps/xor.cpp
+++ apps/xor.cpp
@@ -8,37 +8,27 @@
 #include <vector>
 
 
-template<typename T>
-void new_func_xor(const size_t bach_size, const T learning_rate, const T error_limit) {
-  nnet::MLPerceptron<T> nn1;
-  std::vector<size_t> topology = {2, 3, 3, 1};
+void runXor(const size_t bach_size, const float learning_rate, const float error_limit) {
+  nnet::MLPModel model;
+  auto &nn1 = model.getPerceptron();
+  nnet::MLPTopology topology = {2, 3, 3, 1};
   nn1.setTopology(topology);
   nn1.setActivationFunction(af::ActivationFunctionType::sigmoid);
   nn1.randomizeWeight();
 
-  nnet::MLPerceptron<T> nn2(nn1);
-  nn2.setTopology(topology);
-  nn2.setActivationFunction(af::ActivationFunctionType::sigmoid);
-
   auto &w1 = nn1.getWeights();
-  auto &w2 = nn2.getWeights();
-  for (size_t i = 0; i < w1.size(); i++) w2[i] = w1[i];
 
-  nnet::SGDOptimization<T> tmStandard(0.2f);
-  nnet::MomentumOptimization<T> tmMomentum(topology, 0.1f, 0.9f);
+  auto tmStandard = std::make_shared<nnet::SGDOptimization<float>>(0.2f);
 
-  nnet::MLPModelStochOptimizer<T> opt1(&nn1, &tmStandard);
-  nnet::MLPModelStochOptimizer<T> opt2(&nn2, &tmMomentum);
+  nnet::MLPModelStochOptimizer<float> opt1(model, tmStandard);
 
-  std::cout << nn1 << std::endl;
-  std::cout << nn2 << std::endl;
-
-  std::vector<math::Matrix<T>> input(4);
-  std::vector<math::Matrix<T>> target(4);
+  std::vector<math::FloatMatrix> input(4);
+  std::vector<math::FloatMatrix> target(4);
   for (size_t i = 0; i < 4; i++) {
-    input[i] = math::Matrix<T>(2, 1);
-    target[i] = math::Matrix<T>(1, 1);
+    input[i] = math::FloatMatrix(2, 1);
+    target[i] = math::FloatMatrix(1, 1);
   }
+
   input[0](0, 0) = 0.f;
   input[0](1, 0) = 0.f;
   input[1](0, 0) = 1.f;
@@ -53,52 +43,33 @@ void new_func_xor(const size_t bach_size, const T learning_rate, const T error_l
   target[2](0, 0) = 1.f;
   target[3](0, 0) = 0.f;
 
-  for (int z = 0; z < 2; z++) {
-    T error = 1.0;
-    size_t count = 0;
-    while (error > error_limit && count < 600) {
-      for (int i = 0; i < bach_size; i++) {
-        for (int j = 0; j < 4; j++) {
-          if (z == 0) {
-            opt1.train(input[j], target[j]);
-          } else {
-            opt2.train(input[j], target[j]);
-          }
-        }
-      }
-
-      error = 0.0;
-      for (int i = 0; i < input.size(); i++) {
-        if (z == 0) error += std::fabs(nn1.predict(input[i])(0, 0) - target[i](0, 0));
-        else
-          error += std::fabs(nn2.predict(input[i])(0, 0) - target[i](0, 0));
-      }
-      error /= input.size();
-      std::cout << std::setprecision(17) << error << std::endl;
-      count++;
+  float error = 1.0;
+  size_t count = 0;
+  std::cout << std::setprecision(8);
+  while (error > error_limit && count < 600) {
+    for (int i = 0; i < bach_size; i++) {
+      for (int j = 0; j < 4; j++) { opt1.train(input[j], target[j]); }
     }
 
-    if (z == 0) {
-      std::cout << nn1 << std::endl;
-      std::cout << "Result"
-                << "---> " << count << " iterations" << std::endl;
-      for (int i = 0; i < input.size(); i++) {
-        std::cout << input[i](0, 0) << "|" << input[i](1, 0) << " = " << nn1.predict(input[i])
-                  << "(" << target[i](0, 0) << ")" << std::endl;
-      }
-    } else {
-      std::cout << nn2 << std::endl;
-      std::cout << "Result"
-                << "---> " << count << " iterations" << std::endl;
-      for (int i = 0; i < input.size(); i++) {
-        std::cout << input[i](0, 0) << "|" << input[i](1, 0) << " = " << nn2.predict(input[i])
-                  << "(" << target[i](0, 0) << ")" << std::endl;
-      }
+    error = 0.0;
+    for (int i = 0; i < input.size(); i++) {
+      error += std::fabs(nn1.predict(input[i])(0, 0) - target[i](0, 0));
     }
+    error /= input.size();
+    std::cout << error << std::endl;
+    count++;
+  }
+
+  std::cout << nn1 << std::endl;
+  std::cout << "Result"
+            << "---> " << count << " iterations" << std::endl;
+  for (int i = 0; i < input.size(); i++) {
+    std::cout << input[i](0, 0) << "|" << input[i](1, 0) << " = " << nn1.predict(input[i]) << "("
+              << target[i](0, 0) << ")" << std::endl;
   }
 }
 
 int main() {
-  xor();
+  runXor(100, 0.1, 0.001);
   return 0;
 }
