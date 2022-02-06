c787b5b0c487e56abb2b93d50d6f8a86d62692f1
byjtew93 <benjamin.lozes@protonmail.com>
2022-02-06 22:24:56 +0100

    New interface object to handle C++ stored results.
    TODO: Create a training accordingly to the configuration file

--- interface/main.py
+++ interface/main.py
@@ -1,23 +1,6 @@
-import INTERFACE
-import time
-import array
-import numpy as np
-import sys
+import kreps
 
-
-def createAndTrain(training_parameters):
-    print(f"training_parameters={training_parameters}")
-    # INTERFACE.createAndTrain(training_parameters)
-
-
-if (len(sys.argv) < 3):
-    print(f"Usage: {sys.argv[0]} dataset_path output_path")
-else:
-    training_parameters =  {
-        "dataset_path": sys.argv[1],
-        "output_path" : sys.argv[2],
-        "rescale_size": 32,
-        "preprocess_transformations": [], 
-        "postprocess_transformations": []
-    }
-    createAndTrain(training_parameters)
+c = kreps.NetworkInterface("parameters.json")
+print("Made a networkInterface called !")
+c.printJSONConfig();
+print("InterfaceObject version is: %s" % kreps.getVersion())
--- interface/test.py
+++
@@ -1,3 +0,0 @@
-import kreps
-
-print("InterfaceObject version is: %s" % kreps.getVersion())
