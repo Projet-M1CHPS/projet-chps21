529b23e3e4a283be31466fdbb422365e0e84d2ec
byjtew93 <benjamin.lozes@protonmail.com>
2022-02-11 13:31:06 +0100

    Configuration file bug fix

--- interface/main.py
+++ interface/main.py
@@ -1,6 +1,23 @@
+import sys
+
+# == neural-network interface ==
 import kreps
 
-c = kreps.NetworkInterface("parameters.json")
-print("Made a networkInterface called !")
-c.printJSONConfig();
+
+# == ======================== ==
+
+
+def onPrecisionChanged():
+    print("onPrecisionChanged")
+
+
+if len(sys.argv) < 2:
+    print(f"Usage : python3 {sys.argv[0]} configuration_file_path\nNo configuration file given. \nStop.")
+    exit(0)
+
 print("InterfaceObject version is: %s" % kreps.getVersion())
+c = kreps.NetworkInterface(sys.argv[1])
+print("Made a networkInterface called !")
+c.printJSONConfig()
+# c.onPrecisionChanged(onPrecisionChanged)
+c.createAndTrain()
