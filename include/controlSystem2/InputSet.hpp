5b2d98f5fddb2196af40f550bbe9708f2da0c5a2
Thukisdo <mathys.jam@gmail.com>
2022-03-29 16:44:49 +0200

    Improved InputSet

--- include/controlSystem2/InputSet.hpp
+++ include/controlSystem2/InputSet.hpp
@@ -16,19 +16,16 @@ namespace control {
      * @brief Builds a new sample
      * @param id The unique id of this sample
      * @param class_id The id of the class of this sample
-     * @param path The path of the file this sample was loaded from
      * @param data The raw data of the sample
      */
-    Sample(size_t id, size_t class_id, std::filesystem::path path, math::clFMatrix &&data)
-        : id(id), class_id(class_id), path(std::move(path)), data(std::move(data)) {}
+    Sample(size_t id, size_t class_id, math::clFMatrix &&data)
+        : id(id), class_id(class_id), data(std::move(data)) {}
 
     size_t getId() const { return id; }
 
     size_t getClass() const { return class_id; }
 
     void setClass(size_t class_id) { this->class_id = class_id; }
-
-    const std::filesystem::path &getPath() const { return path; }
 
     /**
      * @brief Returns the opencl buffer containing the data of this sample
@@ -40,9 +37,10 @@ namespace control {
   private:
     size_t id;
     size_t class_id;
-    std::filesystem::path path;
     math::clFMatrix data;
   };
+
+  class InputSetIterator;
 
   /**
    * @brief A set of samples that can be used to feed a neural network model
@@ -51,69 +49,231 @@ namespace control {
    */
   class InputSet {
   public:
+    friend InputSetIterator;
+
     /**
      * @brief Append the given samples and their associated tensor to the input set
      * No check is done to ensure the samples are contained in the tensor, beware that this may
      * leads to undefined behavior.
      *
-     * @param samples The samples to append to this set
      * @param tensor The tensor containing the samples
+     * @param ids A vector containing the ids of the samples to append
+     * @param class_id A vector containing the class ids of the samples to append. If the size of
+     * this vector is smaller than the size of the ids vector, the class id is ignored. This allow
+     * one to create an InputSet where the classes are not known
      */
-    void append(std::vector<Sample> &&samples, math::clFTensor &&tensor);
+    void append(math::clFTensor &&tensor, std::vector<size_t> ids,
+                std::vector<size_t> class_id = {});
 
-    Sample &operator[](size_t index) {
-      if (index > samples.size()) throw std::out_of_range("Sample index out of range");
-      return samples[index];
+    /**
+     * @brief Alter the tensors size and reorder the samples (Maintaining the same ordering) to
+     * match the new tensor size.
+     * This methods is not thread-safe, and invalidates all iterators.
+     * Note that this methods is costly, and should be used with care.
+     *
+     * @param new_tensor_size The new size  of the tensors. If the tensor size is not a multiple of
+     * the number of samples, the last tensor will be truncated. Tensor that already are at the
+     * right size are left untouched
+     */
+    void alterTensors(size_t new_tensor_size);
+
+    size_t getSize() const { return size; }
+
+    Sample operator[](size_t index) {
+      if (index > size) throw std::out_of_range("Sample index out of range");
+
+      size_t tensor_index = index;
+      size_t local_index = index;
+
+      // Since the tensors may not be of the same size, we need to iterate through them to find the
+      // right one
+      for (size_t i = 0; i < tensors.size(); i++) {
+        // If the tensors contains the sample, break
+        if (local_index < tensors[i].getZ()) {
+          tensor_index = i;
+          break;
+        }
+        // Otherwise, update the local index
+        // And move to the next tensor
+        local_index -= tensors[i].getZ();
+      }
+      // Fetch the matrix from the tensor
+      auto matrix = tensors[tensor_index].getMatrix(local_index);
+      // Fetch the sample id/class_id using the index, and return the matrix
+      return {ids[index], class_ids[index], std::move(matrix)};
     }
 
-    const Sample &operator[](size_t index) const {
-      if (index > samples.size()) throw std::out_of_range("Sample index out of range");
-      return samples[index];
-    }
+    using SampleIterator = InputSetIterator;
 
-    std::vector<Sample> &getSamples() { return samples; }
-    const std::vector<Sample> &getSamples() const { return samples; }
+    SampleIterator begin();
 
-    using SampleIterator = std::vector<Sample>::iterator;
-    using SampleConstIterator = std::vector<Sample>::const_iterator;
+    SampleIterator end();
 
-    SampleIterator begin() { return samples.begin(); }
-
-    SampleIterator end() { return samples.end(); }
-
-    SampleConstIterator begin() const { return samples.begin(); }
-
-    SampleConstIterator end() const { return samples.end(); }
-
+    /**
+     * @brief Return the number of tensors contained in this set
+     * Use getSize() to get the number of samples
+     * @return The number of tensors
+     */
     size_t getTensorCount() const { return tensors.size(); }
 
+    /**
+     * @brief Return the tensor at the given index
+     * @param index The index of the tensor to return
+     * @return The tensor at the given index
+     */
     math::clFTensor &getTensor(size_t index) {
       if (index > tensors.size()) throw std::out_of_range("Tensor index out of range");
       return tensors[index];
     }
 
+    /**
+     * @brief Return the tensor at the given index
+     * @param index The index of the tensor to return
+     * @return The tensor at the given index
+     */
     const math::clFTensor &getTensor(size_t index) const {
       if (index > tensors.size()) throw std::out_of_range("Tensor index out of range");
       return tensors[index];
     }
 
-    std::vector<math::clFTensor> &getTensors() { return tensors; }
-    const std::vector<math::clFTensor> &getTensors() const { return tensors; }
-
     using TensorIterator = std::vector<math::clFTensor>::iterator;
     using TensorConstIterator = std::vector<math::clFTensor>::const_iterator;
 
+    /**
+     * @brief Return an iterator to the tensors contained in this set
+     * @return
+     */
     TensorIterator beginTensor() { return tensors.begin(); }
-
-    TensorIterator endTensor() { return tensors.end(); }
-
     TensorConstIterator beginTensor() const { return tensors.begin(); }
 
+    /**
+     * @brief Return an iterator to the end of the tensors contained in this set
+     * @return
+     */
+    TensorIterator endTensor() { return tensors.end(); }
     TensorConstIterator endTensor() const { return tensors.end(); }
 
+    /**
+     * @brief Return the vector containing all the tensors
+     * @return
+     */
+    std::vector<math::clFTensor> &getTensors() { return tensors; }
+
+    /**
+     * @brief Return the vector containing all the tensors
+     * @return
+     */
+    const std::vector<math::clFTensor> &getTensors() const { return tensors; }
+
   private:
-    std::vector<Sample> samples;
     std::vector<math::clFTensor> tensors;
+    size_t size = 0;
+    std::vector<size_t> ids;
+    std::vector<size_t> class_ids;
   };
 
+  /**
+   * @brief Provides an iterator over the samples of an input set
+   * The iterator is a bidirectional iterator, and can be used to iterate over the samples of an
+   * input set with a low overhead.
+   */
+  class InputSetIterator {
+  public:
+    /**
+     * @brief Creates a new iterator on the given input set, starting at the given index
+     * @param parent The input set to iterate over
+     * @param global_index The index of the sample to start the iteration at
+     */
+    InputSetIterator(InputSet &parent, size_t global_index)
+        : parent(&parent), global_index(global_index) {
+      // Find the tensor containing the sample
+      for (size_t i = 0; i < parent.getTensorCount(); i++) {
+        if (global_index < parent.getTensor(i).getZ()) {
+          tensor_index = i;
+          local_index = global_index;
+          break;
+        } else {
+          global_index -= parent.getTensor(i).getZ();
+        }
+      }
+    }
+
+    /**
+     * @brief Return the current sample
+     * @return
+     */
+    Sample operator*() const {
+      if (global_index >= parent->getSize()) throw std::out_of_range("Sample index out of range");
+
+      auto matrix = parent->tensors[tensor_index].getMatrix(local_index);
+      return {parent->ids[global_index], parent->class_ids[global_index], std::move(matrix)};
+    }
+
+    InputSetIterator operator+(long int n) const {
+      InputSetIterator res(*parent, global_index, tensor_index, local_index);
+      return res += n;
+    }
+
+    InputSetIterator operator-(long int n) const {
+      InputSetIterator res(*parent, global_index, tensor_index, local_index);
+      return res -= n;
+    }
+
+    InputSetIterator &operator+=(long int n) {
+      global_index += n;
+      local_index += n;
+      while (local_index >= parent->tensors[tensor_index].getZ()) {
+        local_index -= parent->tensors[tensor_index].getZ();
+        tensor_index++;
+      }
+      return *this;
+    }
+
+    InputSetIterator &operator-=(long int n) {
+      global_index -= n;
+      local_index -= n;
+      while (local_index < 0) {
+        tensor_index--;
+        local_index += parent->tensors[tensor_index].getZ();
+      }
+      return *this;
+    }
+
+    InputSetIterator &operator++() { return *this += 1; }
+
+    InputSetIterator &operator--() { return *this -= 1; }
+
+    bool operator==(const InputSetIterator &other) const {
+      return global_index == other.global_index && tensor_index == other.tensor_index &&
+             local_index == other.local_index;
+    }
+
+    bool operator!=(const InputSetIterator &other) const { return not(*this == other); }
+
+    bool operator<(const InputSetIterator &other) const {
+      return global_index < other.global_index;
+    }
+
+    bool operator>(const InputSetIterator &other) const {
+      return global_index > other.global_index;
+    }
+
+    bool operator<=(const InputSetIterator &other) const {
+      return global_index <= other.global_index;
+    }
+
+    bool operator>=(const InputSetIterator &other) const {
+      return global_index >= other.global_index;
+    }
+
+  private:
+    InputSetIterator(InputSet &parent, size_t global_index, size_t tensor_index, size_t local_index)
+        : parent(&parent), global_index(global_index), tensor_index(tensor_index),
+          local_index(local_index) {}
+
+    InputSet *parent;
+    size_t global_index;
+    size_t local_index;
+    size_t tensor_index;
+  };
 }   // namespace control
--- include/controlSystem2/TrainingCollection.hpp
+++ include/controlSystem2/TrainingCollection.hpp
@@ -3,12 +3,76 @@
 
 
 namespace control {
-  class TrainingCollection {
-  public :
 
-  private :
+  /**
+   * @brief A collection of samples that can be used for training a model. Provides a training set
+   * and an evaluation set.
+   */
+  class TrainingCollection {
+  public:
+    friend class TrainingCollectionLoader;
+
+    // Since copying the collection might be costly, we delete the default copy constructor and
+    // assignment operator to prevent misuse
+    TrainingCollection(const TrainingCollection &other) = delete;
+    TrainingCollection &operator=(const TrainingCollection &other) = delete;
+
+    TrainingCollection &operator=(TrainingCollection &&other) = default;
+
+    InputSet &getTrainingSet() { return training_set; }
+
+    const InputSet &getTrainingSet() const { return training_set; }
+
+    InputSet &getEvaluationSet() { return eval_set; }
+
+    const InputSet &getEvaluationSet() const { return eval_set; }
+
+    size_t getClassCount() const { return class_names.size(); }
+
+    void updateClassNames(const std::vector<std::string> &class_names);
+
+    std::string getClassName(size_t class_index) const {
+      if (class_index >= class_names.size()) {
+        throw std::out_of_range("class index out of range");
+      }
+      return class_names[class_index];
+    }
+
+    const std::vector<std::string> &getClassNames() const { return class_names; }
+
+    /**
+     * @brief Alter the location of every input in both sets to create new tensors of the given
+     * size. Residual elements will be grouped in a smaller tensors at the end of the lists Note
+     * that this is a really costly operation, so it should be used with care.
+     *
+     * @details New tensors will be created with the given size, and the inputs will be moved
+     * (Maintaining the same ordering) to fill them up. Older tensors are deleted, and the last
+     * tensor might be smaller if the number of inputs is not a multiple of the new size.
+     *
+     * @param new_tensor_size The new size for the tensors
+     * @return
+     */
+    size_t alterTensors(size_t new_tensor_size);
+
+    /**
+     * @brief Shuffle the inputs in both sets. Since moving each image individually
+     * would be costly, this operation only shuffle the tensors.
+     *
+     * @param training_seed The random seed to be used for the training set
+     * @param eval_seed The random seed to be used for the evaluation set
+     */
+    void shuffle(size_t training_seed, size_t eval_seed);
+
+    /**
+     * @brief Shuffle the inputs in both sets. Since moving each image individually
+     * would be costly, this operation only shuffle the tensors.
+     */
+    void shuffle();
+
+  private:
     InputSet training_set;
     InputSet eval_set;
+    std::vector<std::string> class_names;
   };
 
 }   // namespace control
