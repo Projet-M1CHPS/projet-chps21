f18e8b9ffc0a249aea4f097d5eb7dbf33d6fc135
byjtew93 <benjamin.lozes@protonmail.com>
2022-05-02 12:49:48 +0200

    Model synchronization small optimization (asynchronicity)
    Done:
    - Model synchronization optimized with asynchronous OpenCL maps
    - Model synchronization optimized with asynchronous MPI broadcasts
    
    Todo:
    - Optimize MPIMLPOptimizer::gatherWeightUpdates with an OpenCL to receive matrices

--- src/neuralNetwork/Perceptron/MPIMLPOptimizer.cpp
+++ src/neuralNetwork/Perceptron/MPIMLPOptimizer.cpp
@@ -128,19 +128,26 @@ namespace nnet {
 
     if (n_process == 1) return;
 
-    // Share & Update weight_updates attribute
-    cl::CommandQueue q = utils::cl_wrapper.getDefaultQueue();
-    // Todo: Vector of pointers & send them one by one, asynchronously
-    for (const auto &new_weights : optimizer->getNeuralNetwork()->getWeights()) {
-      void *data_ptr = q.enqueueMapBuffer(
-              new_weights.getBuffer(), CL_TRUE, rank == 0 ? CL_MAP_READ : CL_MAP_WRITE,
-              new_weights.getOffsetInBytes(), new_weights.sizeInBytes());
+    auto &weights = optimizer->getNeuralNetwork()->getWeights();
+    // Map asynchronous buffer for weight_updates
+    std::vector<void *> data_ptrs(weights.size());
+    data_ptrs.reserve(weights.size());
+    for (size_t i = 0; i < weights.size(); i++)
+      data_ptrs[i] = enqueueMapBuffer(weights[i].getBuffer(), CL_FALSE,
+                                      rank == 0 ? CL_MAP_READ : CL_MAP_WRITE,
+                                      weights[i].getOffsetInBytes(), weights[i].sizeInBytes());
+    utils::cl_wrapper.getDefaultQueue().finish();
 
-      MPI_Bcast(data_ptr, (int) new_weights.size(), MPI_FLOAT, 0, current_comm);
+    // Broadcast weight_updates
+    std::vector<MPI_Request> requests(weights.size());
+    for (size_t i = 0; i < weights.size(); i++)
+      MPI_Ibcast(data_ptrs[i], (int) weights[i].size(), MPI_FLOAT, 0, current_comm, &requests[i]);
+    MPI_Waitall((int) requests.size(), requests.data(), MPI_STATUSES_IGNORE);
 
-      q.enqueueUnmapMemObject(new_weights.getBuffer(), data_ptr);
-    }
-    q.finish();
+    // Unmap buffers
+    for (size_t i = 0; i < weights.size(); i++)
+      enqueueUnmapMemObject(weights[i].getBuffer(), data_ptrs[i]);
+    utils::cl_wrapper.getDefaultQueue().finish();
   }
 
   MPI_Comm MPIMLPOptimizer::Operation::getCommunicator() { return current_comm; }
